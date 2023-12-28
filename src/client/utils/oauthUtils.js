d20e6a5d9a012afbae6d79f0fe3789475474084f
Joey Huntington <jwhunt19@gmail.com>
2023-12-27 20:57:01 -0700

    add comments

--- src/client/utils/oauthUtils.js
+++ src/client/utils/oauthUtils.js
@@ -1,5 +1,6 @@
 // generate code verifier using crypto.getRandomValues
 export function generateCodeVerifier() {
+  // Generate a secure random string using the browser crypto functions
   const generateRandomString = (length) => {
     const possible =
       "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -8,18 +9,19 @@ export function generateCodeVerifier() {
   };
 
   const codeVerifier = generateRandomString(64);
-
   return codeVerifier;
 }
 
 // generate code challenge from code verifier
 export async function generateCodeChallenge(codeVerifier) {
+  // Generate a SHA-256 hash of the input string using SubtleCrypto interface.
   const sha256 = async (plain) => {
     const encoder = new TextEncoder();
     const data = encoder.encode(plain);
     return window.crypto.subtle.digest("SHA-256", data);
   };
 
+  // Base64-urlencodes the input string
   const base64encode = (input) => {
     return btoa(String.fromCharCode(...new Uint8Array(input)))
       .replace(/=/g, "")
@@ -27,6 +29,7 @@ export async function generateCodeChallenge(codeVerifier) {
       .replace(/\//g, "_");
   };
 
+  // Generate code challenge from code verifier according to PKCE spec.
   const hashed = await sha256(codeVerifier);
   const codeChallenge = base64encode(hashed);
 
