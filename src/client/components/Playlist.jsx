0195f3ffd7c674630fa49d17ea40091ebd9b5ccf
Joey Huntington <jwhunt19@gmail.com>
2023-12-28 20:51:32 -0700

    fix crash for empty playlist image array

--- src/client/components/Playlist.jsx
+++ src/client/components/Playlist.jsx
@@ -1,8 +1,18 @@
 const Playlist = ({ playlist, getPlaylistTracks }) => {
+  const checkForImage = (playlist) => {
+    if (playlist.images.length > 0) {
+      return playlist.images[0].url;
+    } else {
+      return "src/client/imgs/playlist_placeholder.png";
+    }
+  };
+
+  let src = checkForImage(playlist);
+
   return (
     <li>
       <div>
-        <img src={playlist.images[0].url} alt={playlist.name}></img>
+        <img src={src} alt={playlist.name}></img>
         <p>{playlist.name}</p>
       </div>
       <button onClick={() => getPlaylistTracks(playlist)}>&darr;</button>
